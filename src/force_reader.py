b0b7a543b1407e8fe97e5d31bd2be88614b5d958
Tom Bavarsky <tomba16062004@gmail.com>
2022-10-18 16:57:25 +0300

    solved delay and converted force to float from str

---
+++ src/force_reader.py
@@ -0,0 +1,36 @@
+import serial.tools.list_ports
+import serial
+
+SENSOR_VID_PID = "VID:PID=0483:5740"
+ARDUINO_VID_PID = "VID:PID=2341:0043"
+
+
+def find_port(VID_PID):
+    for port in serial.tools.list_ports.comports():
+        if VID_PID in port.hwid:
+            return port.device
+
+    return 0
+
+
+def get_force_val(sensor):
+    sensor.reset_input_buffer()
+    sensor.write(b'?')  # requests value
+    bytes_to_read = sensor.in_waiting
+    # sensor_val = sensor.read(bytes_to_read).decode('UTF-8')
+    sensor_val = sensor.read(bytes_to_read).decode('utf-8')
+    if sensor_val != "":
+        return sensor_val
+
+
+def main():
+    force_sensor = serial.Serial(find_port(SENSOR_VID_PID), 115200)
+
+    while True:
+        sensor_val = get_force_val(force_sensor)
+
+        if sensor_val is not None:
+            print(float(sensor_val[:4]))
+
+
+main()
